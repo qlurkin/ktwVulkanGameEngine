6d607516ef4131b2a1b4ab41e9c5feb33c6d83d1
Quentin Lurkin <qlurkin@gmail.com>
2020-11-05 22:36:59 +0100

    Depedencies Tested

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
 # ktwVulkanGameEngine
+
+## Support
+
+Only support Windows for now. We build with the *Build Tools for Visual Studio*
+
+## Build
+
+We use premake5 to create project :
+
+```sh
+premake5 vs2019
+```
+
+then build:
+
+```sh
+MSBuild.exe
+```
---
+++ premake5.lua
@@ -0,0 +1,47 @@
+workspace "ktwVulkanGameEngine"
+	architecture "x64"
+
+	configurations {
+		"Debug",
+		"Release"
+	}
+
+outputdir = "%{cfg.buildcfg}-%{cfg.system}-%{cfg.architecture}"
+
+include "vendor/glfw"
+
+project "ktwVulkanGameEngine"
+	kind "ConsoleApp"
+	language "C++"
+	cppdialect "C++17"
+	staticruntime "on"
+
+	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("bin-obj/" .. outputdir .. "/%{prj.name}")
+
+	files {
+		"src/**.h",
+		"src/**.cpp"
+	}
+
+	includedirs {
+		"src",
+		"vendor/spdlog/include",
+		"vendor/glfw/include",
+		"vendor/glm",
+		"$(VULKAN_SDK)/include"
+	}
+
+	links {
+		"GLFW",
+		"$(VULKAN_SDK)/lib/vulkan-1.lib"
+	}
+
+	filter "system:windows"
+		systemversion "latest"
+
+	filter "configurations:Debug"
+		symbols "on"
+
+	filter "configurations:Release"
+		optimize "on"
